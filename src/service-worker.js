483c8b7f4a176b522ca7c15378868de5009d496d
Anduin Xue <anduin@aiursoft.com>
2024-08-22 09:33:50 +0000

    Update service worker to cache additional resources

--- src/service-worker.js
+++ src/service-worker.js
@@ -16,7 +16,6 @@ const urlsToCache = [
   '/node_modules/aiur-ui-stack/js/app.js'
 ];
 
-// 安装 Service Worker 并缓存资源
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -27,25 +26,20 @@ self.addEventListener('install', event => {
   );
 });
 
-// 处理网络请求，优先从缓存中获取资源
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // 如果资源在缓存中，直接返回
         if (response) {
           return response;
         }
         
-        // 如果资源不在缓存中，进行网络请求
         return fetch(event.request).then(
           response => {
-            // 如果请求失败，返回错误
             if(!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
 
-            // 将新的资源添加到缓存中
             const responseToCache = response.clone();
             caches.open(CACHE_NAME)
               .then(cache => {
@@ -59,7 +53,6 @@ self.addEventListener('fetch', event => {
   );
 });
 
-// 更新 Service Worker，删除旧缓存
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
