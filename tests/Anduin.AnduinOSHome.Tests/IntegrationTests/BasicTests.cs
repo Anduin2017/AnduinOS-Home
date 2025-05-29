33decf96cfbf914f7fdc07c5725b93a242b82df3
Anduin Xue <anduin@aiursoft.com>
2025-05-29 13:28:32 +0000

    Add integration tests for Privacy, Terms, and Versions endpoints

--- tests/Anduin.AnduinOSHome.Tests/IntegrationTests/BasicTests.cs
+++ tests/Anduin.AnduinOSHome.Tests/IntegrationTests/BasicTests.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using Aiursoft.CSTools.Tools;
+using Anduin.AnduinOSHome.Models;
 using AngleSharp.Html.Dom;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Anduin.AnduinOSHome.Tests.Tools;
@@ -50,5 +52,51 @@ public class BasicTests
         Assert.AreEqual("text/html; charset=utf-8", response.Content.Headers.ContentType?.ToString());
         var p = doc.QuerySelector("p.text-lg") as IHtmlElement;
         Assert.IsTrue(p?.InnerHtml.Contains("AnduinOS is a custom Ubuntu-based Linux distribution that aims to"));
+    }
+
+    [TestMethod]
+    [DataRow("/privacy.html")]
+    public async Task GetPrivacy(string url)
+    {
+        var response = await _http.GetAsync(_endpointUrl + url);
+        var doc = await HtmlHelpers.GetDocumentAsync(response);
+
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        Assert.AreEqual("text/html; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+        var h1 = doc.QuerySelector("h1.text-center") as IHtmlElement;
+        Assert.IsTrue(h1?.InnerHtml.Contains("Privacy Statement"));
+    }
+
+    [TestMethod]
+    [DataRow("/terms.html")]
+    public async Task GetTerms(string url)
+    {
+        var response = await _http.GetAsync(_endpointUrl + url);
+        var doc = await HtmlHelpers.GetDocumentAsync(response);
+
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        Assert.AreEqual("text/html; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+        var h1 = doc.QuerySelector("h1.text-center") as IHtmlElement;
+        Assert.IsTrue(h1?.InnerHtml.Contains("Terms of Service"));
+    }
+
+    [TestMethod]
+    public async Task GetVersions()
+    {
+        var response = await _http.GetAsync(_endpointUrl + "/versions.json");
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        var json = await response.Content.ReadAsStringAsync();
+
+        var versions = JsonSerializer.Deserialize<List<VersionInfo>>(json);
+        Assert.IsTrue(versions?.Count > 0);
+        Assert.AreEqual(versions.First().Version, "1.0");
+        Assert.IsFalse(versions.First().IsVisible);
+        Assert.IsFalse(versions.First().LargeCard);
+        Assert.IsFalse(versions.First().Recommended);
+
+        Assert.AreEqual(versions.Last().Version, "1.3");
+        Assert.IsTrue(versions.Last().IsVisible);
+        Assert.IsTrue(versions.Last().LargeCard);
+        Assert.IsTrue(versions.Last().Recommended);
     }
 }
